3a3f36dea2fe7f75aee3a57703f6a42c6a87cdb2
hossein <kolali.hossein@yahoo.com>
2018-03-07 09:30:50 +0330

    Change

--- app/src/main/java/abdolhossein/kolali/getrequest/ActivityArshiv.java
+++ app/src/main/java/abdolhossein/kolali/getrequest/ActivityArshiv.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -40,6 +41,7 @@ public class ActivityArshiv extends AppCompatActivity {
         setContentView(R.layout.activity_arshiv);
         init();
         readData();
+        Log.i("LOG","kolali");
     }
 
     public void init() {
